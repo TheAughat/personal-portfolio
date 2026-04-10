df53629526b1c05fa15603571d5e063a7146b776
Daniel Rodrigues <rcdan22@gmail.com>
2026-04-10 02:16:53 +0100

    insurance for an esoteric future scenario

--- src/components/pages/LandingBackground.js
+++ src/components/pages/LandingBackground.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import DesktopLayout from './DesktopLayout';
 import './space-bg.css';
+import { message } from '../misc/forTheFuture';
 
 function LandingBackground() {
     return (
         <>
             <DesktopLayout/>
             <div className='space-bg' style={{zIndex: -1}}/>
+            {message()}
         </>
     );
 }
